c7db238f516512aba6b05afdaa5e7f87016dba51
Lars Christian Berg <larschristian.berg@bbzsogr.ch>
2018-05-02 09:45:25 +0200

    added views and titles will be dynamically displayed

--- php/index.php
+++ php/index.php
@@ -9,18 +9,21 @@
  */
 header('Content-Type: text/html; charset=iso-8859-1');
 require_once ("class.kontaktData.php");
-require_once ("class.kontaktListe.php");
+require_once ("/class.kontaktListe.php");
 session_start();
 require_once("class.controller.php");
 require_once("../config/config.php");
 require_once("class.basic.php");
-require_once("class.db.php");
-require_once("class.dbKontakte.php");
+require_once("HelperClasses/class.db.php");
+require_once("HelperClasses/class.dbKontakte.php");
 
 $c = new controller("index.htm.php", config::TEMPLATE_PATH );
-$c->registerSubcontroller("kontakt", "Kontaktformular", false);
-$c->registerSubcontroller("liste", "Kontaktliste", false);
-$c->registerSubcontroller("datum", "", true);
+$c->registerSubcontroller("lehrerView", "Lehrer", false);
+$c->registerSubcontroller("schuelerView", "Schüler", false);
+$c->registerSubcontroller("faecherView", "Fächer", false);
+$c->registerSubcontroller("importView", "Import", false);
+
+//$c->registerSubcontroller("datum", "", true);
 $c->dispatch();
 $c->sendOutput();
 
