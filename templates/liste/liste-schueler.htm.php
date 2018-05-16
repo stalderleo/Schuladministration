d41922e5d8fdb2fb56d390c1a74a990ed3a48ba2
Lars Christian Berg <larschristian.berg@bbzsogr.ch>
2018-05-16 14:10:33 +0200

    optimized table

--- templates/liste/liste-schueler.htm.php
+++ templates/liste/liste-schueler.htm.php
@@ -6,10 +6,12 @@
  *
 -->
 <div class="table-container">
+
+
 <table class="table tstacked">
     <thead>
         <tr>
-            <th>Name</th><th>Vorname</th><th>Email<i class="fal fa-expand-alt"></i></th><th>Kürzel</th><th>Status</th>
+            <th>Name</th><th>Vorname</th><th>Email<i title="Vergrössern/Verkleinern" data-extend class="fas fa-expand"></th><th>Kürzel</th><th>Status</th>
         </tr>
     </thead>
     <tbody>
@@ -19,7 +21,7 @@
         <tr>
                 <td data-label="Name"><a href="#"></a><?php echo $s->getName()?></td>
                 <td data-label="Vorname"><?php echo $s->getVorname()?></td>
-                <td data-label="Email"><?php echo $s->getMail()?></td>
+                <td data-label="Email" class="downsize"><?php echo $s->getMail()?></td>
                 <td data-label="Kuerzel"><?php echo $s->getKuerzel() ?></td>
                 <td data-label="Status"><?php echo $s->getStatus()?></td>
             <?php echo $v->editEntry; ?>
@@ -31,7 +33,10 @@
 
 <div class="btn-container">
     <button title="Neuer Schüler" data-toggle="modal" data-target="#student_modal" class="add"><i class="fas fa-graduation-cap"></i></button>
+</button>
+
 </div>
+
 
 <?php include $this->template_path.'/modals/modal-schueler.html'; ?>
 <?php include $this->template_path.'/modals/modal-klasse.html'; ?>
