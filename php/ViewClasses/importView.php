0fdb22d99653ff90834f500ed0b25fa8a7edd7ca
Leo Stalder <stalderleo@gmail.com>
2018-05-25 15:17:35 +0200

    import funktioniert

--- php/ViewClasses/importView.php
+++ php/ViewClasses/importView.php
@@ -46,13 +46,19 @@ class importView implements subcontroller {
 
     public function run() {
         if (isset($this->params['submit'])) {
-            
-            // Allow certain file formats
-            if(strtolower(pathinfo($_FILES['dataExport']['name'],PATHINFO_EXTENSION)) != "xml") {
-                $this->errorimport = "Sorry, Nur XML-Datei sind zugelassen";
+            if (!empty($_FILES['dataExport']['name'])) {
+                // Allow certain file formats
+                if(strtolower(pathinfo($_FILES['dataExport']['name'],PATHINFO_EXTENSION)) != "xml") {
+                    $this->errorimport = "Sorry, Nur XML-Datei sind zugelassen";
+                }
+
+                if (!empty($this->dbLehrer->selectAllLehrer())) { //Schauen ob DB leer ist
+                    $this->errorimport = "Datenbank muss lehr sein!";
+                }
+                $this->target = $_FILES['dataExport']['tmp_name'];
+            } else {
+                $this->errorimport = "Wählen Sie ein File!";
             }
-            
-            $this->target = $_FILES['dataExport']['tmp_name'];
         }
         
     }
@@ -62,7 +68,7 @@ class importView implements subcontroller {
         include($this->template_path."/"."dataimport.htm.php");
         ob_flush();
         flush();
-        if (isset($this->target)) $this->insertIntoDB($this->target);
+        if (isset($this->target) && empty($this->errorimport)) $this->insertIntoDB($this->target);
     }
     
     /**
@@ -71,101 +77,99 @@ class importView implements subcontroller {
      * @param type $target 
      */
     private function insertIntoDB($target) {
-        if (empty($this->dbLehrer->selectAllLehrer())) { //Schauen ob DB leer ist
-            ini_set('max_execution_time', 600);
+        
+        ini_set('max_execution_time', 600);
 
-            $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Import started"."<hr>\n");
-            $xml = simplexml_load_file($target);
-            foreach ($xml->lehrer as $lp) {   
-                $lehrer = $this->dbLehrer->insertLehrer(new lehrer(str_replace('gibsso','',$lp->id), $lp->username, $lp->initpw, $lp->name, $lp->vorname, $this->convertStringToDate($lp->geburtsdatum), $lp->geschlecht, $lp->kuerzel, $lp->mail, $lp->status));
-                if (!empty($lehrer)) {
-                    $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Lehrer (".$lehrer->getPid().") inserted"."<hr>\n");
+        $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Import started"."<hr>\n");
+        $xml = simplexml_load_file($target);
+        foreach ($xml->lehrer as $lp) {   
+            $lehrer = $this->dbLehrer->insertLehrer(new lehrer(str_replace('gibsso','',$lp->id), $lp->username, $lp->initpw, $lp->name, $lp->vorname, $this->convertStringToDate($lp->geburtsdatum), $lp->geschlecht, $lp->kuerzel, $lp->mail, $lp->status));
+            if (!empty($lehrer)) {
+                $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Lehrer (".$lehrer->getPid().") inserted"."<hr>\n");
+            } else {
+                $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Lehrer ($lp->id) insert failed"."<hr>\n");
+            }
+
+            //Klassen des Lehrers einfügen
+            foreach ($lp->regelklassen->klasse as $klasse) {
+                $klasse = $this->dbKlasse->insertKlasseAI(new klasse($klasse->klasse_kuerzel, $klasse->klasse_bezeichnung));
+                if (!empty($klasse)) {
+                    $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Klasse (".$lehrer->getPid().") inserted"."<hr>\n");
                 } else {
-                    $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Lehrer ($lp->id) insert failed"."<hr>\n");
+                    $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Klasse ($klasse->klasse_kuerzel) insert failed"."<hr>\n");
                 }
+            }
 
-                //Klassen des Lehrers einfügen
-                foreach ($lp->regelklassen->klasse as $klasse) {
-                    $klasse = $this->dbKlasse->insertKlasseAI(new klasse($klasse->klasse_kuerzel, $klasse->klasse_bezeichnung));
-                    if (!empty($klasse)) {
-                        $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Klasse (".$lehrer->getPid().") inserted"."<hr>\n");
-                    } else {
-                        $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Klasse ($klasse->klasse_kuerzel) insert failed"."<hr>\n");
-                    }
-                }
+            foreach ($lp->kurse->kurs as $ks) {
+                if (strpos($ks->kurs_kuerzel, ',') !== false) { 
+                    $kuerzelstr = explode('-', $ks->kurs_kuerzel)[1]; //Mittlerer Teil nehmen
 
-                foreach ($lp->kurse->kurs as $ks) {
-                    if (strpos($ks->kurs_kuerzel, ',') !== false) { 
-                        $kuerzelstr = explode('-', $ks->kurs_kuerzel)[1]; //Mittlerer Teil nehmen
+                    $klassenkuerzelarr = explode(',', $kuerzelstr);
+                    foreach ($klassenkuerzelarr as $klassenkuerzel) {
+                        $klasse = $this->dbKlasse->selectKlasseByKuerzel($klassenkuerzel);
+                        if (empty($klasse)) {
+                            $this->writeToLog(date('m.d.Y h:i:s a', time()). ": failed finding klass (".$klassenkuerzel.")"."<hr>\n");
+                        }
+                        $kurs = $this->dbKurs->insertKursAI(new kurs($ks->kurs_kuerzel, $ks->kurs_bezeichnung));
+                        if (!empty($kurs)) {
+                            $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Kurs (".$kurs->getFid().", " .$lp->username .") inserted"."<hr>\n");
+                        } else {
+                            $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Kurs ($kurs->kurs_kuerzel) insert failed"."<hr>\n");
+                        }
 
-                        $klassenkuerzelarr = explode(',', $kuerzelstr);
-                        foreach ($klassenkuerzelarr as $klassenkuerzel) {
-                            $klasse = $this->dbKlasse->selectKlasseByKuerzel($klassenkuerzel);
-                            if (empty($klasse)) {
-                                $this->writeToLog(date('m.d.Y h:i:s a', time()). ": failed finding klass (".$klassenkuerzel.")"."<hr>\n");
-                            }
-                            $kurs = $this->dbKurs->insertKursAI(new kurs($ks->kurs_kuerzel, $ks->kurs_bezeichnung));
-                            if (!empty($kurs)) {
-                                $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Kurs (".$kurs->getFid().", " .$lp->username .") inserted"."<hr>\n");
-                            } else {
-                                $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Kurs ($kurs->kurs_kuerzel) insert failed"."<hr>\n");
-                            }
+                        $kursinstanz = $this->dbKursinstanz->insertInstanz(new kursInstanz($lehrer, $klasse, $kurs));
 
-                            $kursinstanz = $this->dbKursinstanz->insertInstanz(new kursInstanz($lehrer, $klasse, $kurs));
-
-                            if (!empty($kursinstanz)) {
-                                $this->writeToLog( date('m.d.Y h:i:s a', time()). ": Kursinstanz (".$kursinstanz->getLehrer()->getPid().", ".$kursinstanz->getKlasse()->getKid().", ".$kursinstanz->getKurs()->getFid().") inserted"."<hr>\n");
-                            } else {
-                                $this->writeToLog( date('m.d.Y h:i:s a', time()). ": Kursinstanz ($klasse->klasse_kuerzel) insert failed"."<hr>\n") ;
-                            }
+                        if (!empty($kursinstanz)) {
+                            $this->writeToLog( date('m.d.Y h:i:s a', time()). ": Kursinstanz (".$kursinstanz->getLehrer()->getPid().", ".$kursinstanz->getKlasse()->getKid().", ".$kursinstanz->getKurs()->getFid().") inserted"."<hr>\n");
+                        } else {
+                            $this->writeToLog( date('m.d.Y h:i:s a', time()). ": Kursinstanz ($klasse->klasse_kuerzel) insert failed"."<hr>\n") ;
                         }
                     }
-                } 
-            }
-
-            foreach ($xml->schueler as $sl) {
-                $schueler = $this->dbSchueler->insertSchueler(new schueler(str_replace('gibsso','',$sl->id), $sl->username, $sl->initpw, $sl->name, $sl->vorname, $this->convertStringToDate($sl->geburtsdatum), $sl->geschlecht, $sl->kuerzel, $sl->mail, $sl->status));
-
-                if (!empty($schueler)) {
-                    $this->writeToLog( date('m.d.Y h:i:s a', time()). ": Schueler (".$schueler->getPid().") inserted"."<hr>\n");
-                } else {
-                    $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Schueler ($sl->id) insert failed"."<hr>\n");
                 }
-
-                $klasse = $this->dbKlasse->selectKlasseByBezeichnung($sl->profile->profil->stammklasse);
-                if (empty($klasse)) {
-                    $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Warning couldn't find klass (".$sl->profile->profil->ausbildung_kuerzel.")"."<hr>\n");
-                } else {
-                    $this->dbKlassenBesuch->insertBesuch(new klassenBesuch($schueler, $klasse, false));
-                }
-
-
-                if (isset($sl->profile->profil->zweitausbildung_kuerzel)) {
-                    $klasse = $this->dbKlasse->selectKlasseByBezeichnung($sl->profile->profil->zweitausbildung_stammklasse);
-                    if (empty($klasse)) {
-                        $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Warning couldn't find zweitausbildungklass (".$sl->profile->profil->zweitausbildung_kuerzel.")"."<hr>\n");
-                    } else {
-                        $this->dbKlassenBesuch->insertBesuch(new klassenBesuch($schueler, $klasse, true));
-                    }
-
-                }
-            }
-
-            foreach ($xml->angestellte as $ag) {
-                $angestellter = $this->dbAngestellte->insertAngestellter(new angestellter(str_replace('gibsso','',$ag->id), $ag->username, $ag->initpw, $ag->name, $ag->vorname, $this->convertStringToDate($ag->geburtsdatum), $ag->geschlecht, $ag->kuerzel, $ag->mail, $ag->status));
-                if (!empty($angestellter)) {
-                    $this->writeToLog( date('m.d.Y h:i:s a', time()). ": Angestellter (".$angestellter->getPid().") inserted"."<hr>\n");
-                } else {
-                    $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Angestellter ($ag->id) insert failed"."<hr>\n");
-                }
-            }
-
-            $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Import abgeschlossen"."<hr>\n");
-            sleep(500);
-            unlink("../res/data/log.txt"); //Logfile nach import löschen
-        } else {
-            $this->errorimport = "Datenbank muss lehr sein!";
+            } 
         }
+
+        foreach ($xml->schueler as $sl) {
+            $schueler = $this->dbSchueler->insertSchueler(new schueler(str_replace('gibsso','',$sl->id), $sl->username, $sl->initpw, $sl->name, $sl->vorname, $this->convertStringToDate($sl->geburtsdatum), $sl->geschlecht, $sl->kuerzel, $sl->mail, $sl->status));
+
+            if (!empty($schueler)) {
+                $this->writeToLog( date('m.d.Y h:i:s a', time()). ": Schueler (".$schueler->getPid().") inserted"."<hr>\n");
+            } else {
+                $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Schueler ($sl->id) insert failed"."<hr>\n");
+            }
+
+            $klasse = $this->dbKlasse->selectKlasseByBezeichnung($sl->profile->profil->stammklasse);
+            if (empty($klasse)) {
+                $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Warning couldn't find klass (".$sl->profile->profil->ausbildung_kuerzel.")"."<hr>\n");
+            } else {
+                $this->dbKlassenBesuch->insertBesuch(new klassenBesuch($schueler, $klasse, false));
+            }
+
+
+            if (isset($sl->profile->profil->zweitausbildung_kuerzel)) {
+                $klasse = $this->dbKlasse->selectKlasseByBezeichnung($sl->profile->profil->zweitausbildung_stammklasse);
+                if (empty($klasse)) {
+                    $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Warning couldn't find zweitausbildungklass (".$sl->profile->profil->zweitausbildung_kuerzel.")"."<hr>\n");
+                } else {
+                    $this->dbKlassenBesuch->insertBesuch(new klassenBesuch($schueler, $klasse, true));
+                }
+
+            }
+        }
+
+        foreach ($xml->angestellte as $ag) {
+            $angestellter = $this->dbAngestellte->insertAngestellter(new angestellter(str_replace('gibsso','',$ag->id), $ag->username, $ag->initpw, $ag->name, $ag->vorname, $this->convertStringToDate($ag->geburtsdatum), $ag->geschlecht, $ag->kuerzel, $ag->mail, $ag->status));
+            if (!empty($angestellter)) {
+                $this->writeToLog( date('m.d.Y h:i:s a', time()). ": Angestellter (".$angestellter->getPid().") inserted"."<hr>\n");
+            } else {
+                $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Angestellter ($ag->id) insert failed"."<hr>\n");
+            }
+        }
+
+        $this->writeToLog(date('m.d.Y h:i:s a', time()). ": Import abgeschlossen"."<hr>\n");
+        sleep(1);
+        unlink("../res/data/log.txt"); //Logfile nach import löschen
+        unset($this->target);
     }
     
     function convertStringToDate( $string )
--- templates/dataimport.htm.php
+++ templates/dataimport.htm.php
@@ -5,10 +5,16 @@
  * Template für den Datenimport
  *
 -->
-<?php if (isset($_FILES['dataExport'])) : ?>
+<?php if (isset($this->target) === TRUE) : ?>
     <script type="text/javascript">
+        
+        var interval = setInterval(function() {
+            updateScroll();
+        },50);
+        
         if(typeof(EventSource) !== "undefined") {
-            var source = new EventSource("import_progress.php").addEventListener('message', function(e) {
+            var source = new EventSource("import_progress.php");
+            source.addEventListener('message', function(e) {
                 if (!(e.data.match("Import abgeschlossen"))) {
                     if (e.data.includes("inserted")) { 
                         document.getElementById("log").innerHTML += "<span class=\"inserted\">"+e.data+"</span>";
@@ -19,8 +25,10 @@
                         document.getElementById("log").innerHTML += "<span class=\"failed\">"+e.data+"</span>";
                     }
                 } else {
-                    this.target.close();
                     document.getElementById("log").innerHTML += "<span><strong>Import completet</strong></span>";
+                    source.close();
+                    clearInterval(interval);
+                    updateScroll();
                 }
             }, false);
         } else {
@@ -31,13 +39,6 @@
             var element = document.getElementById("log");
             element.scrollTop = element.scrollHeight;
         }
-        
-        var scrolled = false;
-        setInterval(function() {
-            if (!scrolled) updateScroll();
-        },50);
-        
-        
     </script>
 <?php endif ?>
 <form enctype="multipart/form-data" action="<?php echo $_SERVER['SCRIPT_NAME']."?id=importView" ?>" method="post" id="importDataForm">
@@ -47,10 +48,9 @@
         <input name="submit" type="submit" value="Hochladen">
     </div>
 </form>
-    <?=
-var_dump($v->errorimport)?>
-<?php if (isset($v->errorimport)) { 
-    echo "<div id=\"error\">"+$v['errorimport']+"</div>"; 
+<?php if (!empty($this->errorimport)) { 
+    echo "<div id=\"error\">".$this->errorimport."</div>"; 
 } else { ?>
     <div id="log" ></div>
 <?php } ?>
+
