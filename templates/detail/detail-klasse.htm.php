77f8232f5545a0fda41436b67fda551be04d7325
taastaa1 <aaron.studer@swisscom.com>
2018-05-27 19:40:24 +0200

    final last changes

--- templates/detail/detail-klasse.htm.php
+++ templates/detail/detail-klasse.htm.php
@@ -27,7 +27,7 @@
 	<table id="" class="table tstacked">
 		<thead>
 			<tr>
-				<th>Fach</th><th>Lehrer</th><th>Löschen</th><th>Detail (Lehrer)</th>
+				<th>Fach</th><th>Lehrer</th>
 			</tr>
 		</thead>
 		<tbody>
@@ -35,17 +35,6 @@
 			<tr>
 				<td><?=  $fach->getBezeichnung(); ?></td>
 				<td><?= $v->lehrer[$index]->getUsername(); ?></td>
-				<td>
-					<form class="delete" method="post">
-						<i class="fas fa-trash"></i><input type="submit" name="del_kurs-klasse" value="<?= $index ?>">
-						<input type="hidden" name="kid" value="<?= $v->klasse->getKid(); ?>">
-					</form>
-				</td>
-				<td>
-					<form class="edit" action="<?php echo $_SERVER['SCRIPT_NAME']."?id=lehrerView" ?>" method="post">
-						<i class="fas fa-edit"></i><input type="submit" name="pid" value="<?= $v->lehrer[$index]->getPid(); ?>">
-					</form>
-				</td>
 			</tr>
 		<?php endforeach; ?>
 		</tbody>
