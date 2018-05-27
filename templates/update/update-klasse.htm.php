5e04efa7b70b81f4ecd8d7fe5c2824febcbba99d
taastaa1 <aaron.studer@swisscom.com>
2018-05-27 18:55:43 +0200

    update for merge

--- templates/update/update-klasse.htm.php
+++ templates/update/update-klasse.htm.php
@@ -1,39 +1,24 @@
-<form method="post">
     <label>Bezeichnung
-    <input type="text" name="k_bez" value="<?= $v->klasse->getBezeichnung(); ?>"></label>
+        <label type="text" name="k_bez" value=""><?= $v->klasse->getBezeichnung(); ?></label></label><br>
     <label>Kürzel
-    <input type="text" name="k_kur" value="<?= $v->klasse->getKuerzel(); ?>"></label>
-    <input type="hidden" name="kid" value="<?= $v->klasse->getKid() ?>">
-    <input type="submit" value="Speichern" name="setKurs">
-</form>
+        <label type="text" name="k_kur" value=""><?= $v->klasse->getKuerzel(); ?></label></label>
 <?php  ?>
 <div class="row margin-50">
 	<h2>Schueler</h2>
 	<table id="" class="table tstacked">
 		<thead>
 			<tr>
-				<th>Benutzername</th><th>Löschen</th><th>Detail</th>
+				<th>Benutzername</th>
 			</tr>
 		</thead>
 		<tbody>
 		<?php foreach ( $v->schueler as $schueler ): ?>
 			<tr>
 				<td><?=  $schueler->getSchueler()->getUsername() ?></td>
-				<td>
-					<form class="delete" method="post">
-						<i class="fas fa-trash"></i><input type="submit" name="del_schueler-klasse" value="<?= $schueler->getSchueler()->getPid(); ?>">
-						<input type="hidden" name="kid" value="<?= $v->klasse->getKid() ?>">
-					</form>
-				</td>
-				<td>
-					<form class="edit" action="<?php echo $_SERVER['SCRIPT_NAME']."?id=schuelerView" ?>" method="post">
-						<i class="fas fa-edit"></i><input type="submit" name="pid" value="<?= $schueler->getSchueler()->getPid(); ?>">
-					</form>
-				</td>
 			</tr>
 		<?php endforeach; ?>
 		</tbody>
-	</table>
+	</table><br>
 
 	<?php if(!empty($v->lehrer)):?>
 
@@ -41,28 +26,17 @@
 	<table id="" class="table tstacked">
 		<thead>
 			<tr>
-				<th>Benutzername</th><th>Löschen</th><th>Detail</th>
+				<th>Benutzername</th>
 			</tr>
 		</thead>
 		<tbody>
 		<?php foreach ( $v->lehrer as $lehrer ): ?>
 			<tr>
 				<td><?= $lehrer->getUsername(); ?></td>
-				<td>
-					<form class="delete" method="post">
-						<i class="fas fa-trash"></i><input type="submit" name="del_lehrer-klasse" value="<?= $lehrer->getPid(); ?>">
-						<input type="hidden" name="kid" value="<?= $v->klasse->getKid(); ?>">
-					</form>
-				</td>
-				<td>
-					<form class="edit" action="<?php echo $_SERVER['SCRIPT_NAME']."?id=lehrerView" ?>" method="post">
-						<i class="fas fa-edit"></i><input type="submit" name="pid" value="<?= $lehrer->getPid(); ?>">
-					</form>
-				</td>
 			</tr>
 		<?php endforeach; ?>
 		</tbody>
-	</table>
+	</table><br>
 
 	<?php endif; ?>
 	<?php if(!empty($v->kurse)):?>
@@ -71,19 +45,13 @@
 	<table id="" class="table tstacked">
 		<thead>
 			<tr>
-				<th>Bezeichnung</th><th>Löschen</th>
+				<th>Bezeichnung</th>
 			</tr>
 		</thead>
 		<tbody>
 		<?php foreach ( $v->kurse as $fach ): ?>
 			<tr>
 				<td><?=  $fach->getBezeichnung(); ?></td>
-				<td>
-					<form class="delete" method="post">
-						<i class="fas fa-trash"></i><input type="submit" name="del_schueler-klasse" value="<?= $fach->getFid(); ?>">
-						<input type="hidden" name="kid" value="<?= $v->klasse->getKid() ?>">
-					</form>
-				</td>
 			</tr>
 		<?php endforeach; ?>
 		</tbody>
